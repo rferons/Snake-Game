ac8305834df10d9cbbb188f066edb3e8e008d331
Ryan Ferons <rferons@mycora.us>
2024-02-11 13:49:24 -0500

    renamed some variable to make it easier for kids to learn

--- snake.py
+++ snake.py
@@ -5,20 +5,18 @@ import random
 import tkinter as tk
 from tkinter import messagebox
 
+# Game board width and height
 width = 500
 height = 500
 
-cols = 25
-rows = 20
-
+# cube size
+cubeSize = 20
 
 class cube():
-    rows = 20
-    w = 500
     def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
         self.pos = start
         self.dirnx = dirnx
-        self.dirny = dirny # "L", "R", "U", "D"
+        self.dirny = dirny
         self.color = color
 
     def move(self, dirnx, dirny):
@@ -28,7 +26,7 @@ class cube():
             
 
     def draw(self, surface, eyes=False):
-        dis = self.w // self.rows
+        dis = width // cubeSize
         i = self.pos[0]
         j = self.pos[1]
         
@@ -40,7 +38,6 @@ class cube():
             circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
             pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
             pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
-        
 
 
 class snake():
@@ -59,6 +56,7 @@ class snake():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+
             keys = pygame.key.get_pressed()
 
             for key in keys:
@@ -79,15 +77,15 @@ class snake():
                     self.dirnx = 0
                     self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
         
-        for i, c in enumerate(self.body):
-            p = c.pos[:]
-            if p in self.turns:
-                turn = self.turns[p]
-                c.move(turn[0], turn[1])
-                if i == len(self.body)-1:
-                    self.turns.pop(p)
+        for index, bodyPart in enumerate(self.body):
+            position = bodyPart.pos[:]
+            if position in self.turns:
+                turn = self.turns[position]
+                bodyPart.move(turn[0], turn[1])
+                if index == len(self.body)-1:
+                    self.turns.pop(position)
             else:
-                c.move(c.dirnx,c.dirny)
+                bodyPart.move(bodyPart.dirnx,bodyPart.dirny)
         
         
     def reset(self,pos):
@@ -115,36 +113,38 @@ class snake():
         self.body[-1].dirny = dy
     
     def draw(self, surface):
-        for i,c in enumerate(self.body):
+        for i,bodyPart in enumerate(self.body):
             if i == 0:
-                c.draw(surface, True)
+                bodyPart.draw(surface, True)
             else:
-                c.draw(surface)
+                bodyPart.draw(surface)
 
 
 
 def redrawWindow():
     global win
     win.fill((0,0,0))
-    drawGrid(width, rows, win)
-    s.draw(win)
+    # drawGrid(width, rows, win)
+    snake.draw(win)
     snack.draw(win)
     pygame.display.update()
     pass
 
 
 
-def drawGrid(w, rows, surface):
-    sizeBtwn = w // rows
+def drawGrid(width, height, surface):
+    # // = integer division
+    # / = floating point division (decimals)
+    sizeBtwn = width // height
 
     x = 0
     y = 0
-    for l in range(rows):
+    for l in range(height):
         x = x + sizeBtwn
         y = y +sizeBtwn
 
-        pygame.draw.line(surface, (255,255,255), (x, 0),(x,w))
-        pygame.draw.line(surface, (255,255,255), (0, y),(w,y))
+        pygame.draw.line(surface, (255,255,255), (x, 0),(x,width))
+        pygame.draw.line(surface, (255,255,255), (0, y),(width,y))
     
 
 
@@ -163,36 +163,34 @@ def randomSnack(rows, item):
 
 
 def main():
-    global s, snack, win
+    global snake, snack, win
     win = pygame.display.set_mode((width,height))
-    s = snake((255,0,0), (10,10))
-    s.addCube()
-    snack = cube(randomSnack(rows,s), color=(0,255,0))
+    snake = snake((255,0,0), (10,10))
+    snake.addCube()
+    snack = cube(randomSnack(cubeSize,snake), color=(0,255,0))
     flag = True
     clock = pygame.time.Clock()
     
     while flag:
         pygame.time.delay(50)
         clock.tick(10)
-        s.move()
-        headPos = s.head.pos
+        snake.move()
+        headPos = snake.head.pos
         if headPos[0] >= 20 or headPos[0] < 0 or headPos[1] >= 20 or headPos[1] < 0:
-            print("Score:", len(s.body))
-            s.reset((10, 10))
+            print("Score:", len(snake.body))
+            snake.reset((10, 10))
 
-        if s.body[0].pos == snack.pos:
-            s.addCube()
-            snack = cube(randomSnack(rows,s), color=(0,255,0))
+        if snake.body[0].pos == snack.pos:
+            snake.addCube()
+            snack = cube(randomSnack(cubeSize,snake), color=(0,255,0))
             
-        for x in range(len(s.body)):
-            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
-                print("Score:", len(s.body))
-                s.reset((10,10))
+        for x in range(len(snake.body)):
+            if snake.body[x].pos in list(map(lambda z:z.pos,snake.body[x+1:])):
+                print("Score:", len(snake.body))
+                snake.reset((10,10))
                 break
                     
         redrawWindow()
 
 main()
-    
-
-    
+     
